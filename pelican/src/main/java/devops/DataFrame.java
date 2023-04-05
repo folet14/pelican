68c46fea9f2a15474b00ab96b7f29bfb324d418b
jbournat <Jean-Edouard.Bournat-Querat@etu.univ-grenoble-alpes.fr>
2023-04-05 10:24:02 +0200

    basic maven frame

---
+++ pelican/src/main/java/devops/DataFrame.java
@@ -0,0 +1,13 @@
+package devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class DataFrame 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
