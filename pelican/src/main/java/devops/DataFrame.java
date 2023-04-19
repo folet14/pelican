6d5e62d896c23514064e083385fe6447be57d507
Hô Dac Gia Phúc <hod@f216-19.e-im2ag.ujf-grenoble.fr>
2023-04-19 10:10:35 +0200

    premiere construction de dataframe

--- pelican/src/main/java/devops/DataFrame.java
+++ pelican/src/main/java/devops/DataFrame.java
@@ -6,8 +6,28 @@ package devops;
  */
 public class DataFrame 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    String[] colonne;
+    String[][] ligne;
+    
+    DataFrame(String[] colonne, String[][] ligne){
+        this.colonne = colonne;
+        this.ligne = new String[ligne.length][];
+        for (int i=0; i< colonne.length; i++){
+        	this.ligne[i] = new String[ligne[i].length];
+            for(int j = 0; j<ligne[i].length; j++) {
+            	this.ligne[i][j] = ligne[i][j];
+            }
+            System.out.print("  "+colonne[i]);
+        }
+        System.out.println();
+        for (int j=0; j< ligne[0].length; j++){
+            System.out.print(j+" ");
+            for (int i=0; i< colonne.length; i++){
+            System.out.print(ligne[j][i]+" ");
+            }
+            System.out.println();
+        }
+        
     }
+    
 }
