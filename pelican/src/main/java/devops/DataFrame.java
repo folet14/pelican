9100334b875fe6fb824c59252a3c75213ae1eca4
Jean-Edouard <Jean-Edouard.Bournat-Querat@etu.univ-grenoble-alpes.fr>
2023-04-26 23:41:04 +0200

    tests all done, all passing, gg ez

--- pelican/src/main/java/devops/DataFrame.java
+++ pelican/src/main/java/devops/DataFrame.java
@@ -1,32 +1,50 @@
 package devops;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class DataFrame {
 
-	private ArrayList<ArrayList<Object>> data;
+	private ArrayList<ArrayList<String>> data;
 	private ArrayList<String> labels;
 
-	public DataFrame(ArrayList<ArrayList<Object>> table, ArrayList<String> labels) {
+	public DataFrame(ArrayList<ArrayList<String>> table, ArrayList<String> labels) {
 		this.data = table;
 		this.labels = labels;
 
 	}
 
-	private ArrayList<Object> getColumn(int index) {
-		ArrayList<Object> column = new ArrayList<>();
-		for (ArrayList<Object> row : data) {
+	public DataFrame(String filePath) throws IOException {
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String[] colonne = reader.readLine().split(",");
+        String lignetmp;
+        for (int i = 0; i < colonne.length; i++) {
+            labels.add(colonne[i]);
+        }
+
+        while ((lignetmp = reader.readLine()) != null) {
+            String[] ligne = lignetmp.split(",");
+            ArrayList<String> row = new ArrayList<>();
+            for (int i = 0; i < ligne.length; i++) {
+                row.add(ligne[i]);
+            }
+            table.add(row);
+        }
+        reader.close();
+        this.data = table;
+    }
+	public ArrayList<String> getColumn(int index) {
+		ArrayList<String> column = new ArrayList<>();
+		for (ArrayList<String> row : data) {
 			column.add(row.get(index));
 		}
 		return column;
 	}
 
-	public ArrayList<Object> getLigne(int index) {
+	public ArrayList<String> getLigne(int index) {
 		return data.get(index);
 	}
 
@@ -34,20 +52,20 @@ public class DataFrame {
 		return labels.get(index);
 	}
 
-	public void addRow(ArrayList<Object> values) {
+	public void addRow(ArrayList<String> values) {
 		data.add(values);
 	}
 
 	public void afficherAll() {
 		System.out.print(" ");
-		for (Object l : labels) {
+		for (String l : labels) {
 			System.out.print(" " + l);
 		}
 		System.out.println();
 		int i = 0;
-		for (ArrayList<Object> arrayList : data) {
+		for (ArrayList<String> arrayList : data) {
 			System.out.print(i);
-			for (Object a : arrayList) {
+			for (String a : arrayList) {
 				System.out.print(" " + a);
 			}
 			System.out.println();
@@ -57,15 +75,15 @@ public class DataFrame {
 
 	public void afficherPremiereLigne(int ligne) {
 		System.out.print(" ");
-		for (Object l : labels) {
+		for (String l : labels) {
 			System.out.print(" " + l);
 		}
 		System.out.println();
 
 		for (int i = 0; i < ligne; i++) {
-			ArrayList<Object> arrayList = data.get(i);
+			ArrayList<String> arrayList = data.get(i);
 			System.out.print(i);
-			for (Object a : arrayList) {
+			for (String a : arrayList) {
 				System.out.print(" " + a);
 			}
 			System.out.println();
@@ -74,15 +92,15 @@ public class DataFrame {
 
 	public void afficherDerniereLigne(int ligne) {
 		System.out.print(" ");
-		for (Object l : labels) {
+		for (String l : labels) {
 			System.out.print(" " + l);
 		}
 		System.out.println();
 
 		for (int i = data.size() - ligne; i < data.size(); i++) {
-			ArrayList<Object> arrayList = data.get(i);
+			ArrayList<String> arrayList = data.get(i);
 			System.out.print(i);
-			for (Object a : arrayList) {
+			for (String a : arrayList) {
 				System.out.print(" " + a);
 			}
 			System.out.println();
@@ -90,7 +108,7 @@ public class DataFrame {
 	}
 
 	public DataFrame selectionLignes(ArrayList<Integer> lignes) {
-		ArrayList<ArrayList<Object>> dt = new ArrayList<>();
+		ArrayList<ArrayList<String>> dt = new ArrayList<>();
 		for (Integer integer : lignes) {
 			dt.add(data.get(integer));
 		}
@@ -98,14 +116,14 @@ public class DataFrame {
 	}
 
 	public DataFrame selectionColonne(ArrayList<String> col) {
-		ArrayList<ArrayList<Object>> dt = new ArrayList<>();
+		ArrayList<ArrayList<String>> dt = new ArrayList<>();
 		int count = 0;
 		for (int i = 0; i < labels.size(); i++) {
 			if (col.contains(labels.get(i))) {
-				ArrayList<Object> cl = getColumn(i);
+				ArrayList<String> cl = getColumn(i);
 				for (int j = 0; j < cl.size(); j++) {
 					if (count == 0) {
-						ArrayList<Object> l = new ArrayList<Object>();
+						ArrayList<String> l = new ArrayList<String>();
 						l.add(cl.get(j));
 						dt.add(l);
 					} else {
@@ -124,7 +142,7 @@ public class DataFrame {
 		if (colonne!= null) {
 			for (int i = 0; i < labels.size(); i++) {
 				if (colonne.contains(labels.get(i))) {
-					for (Object value : getColumn(i)) {
+					for (String value : getColumn(i)) {
 						if (Double.valueOf((String) value) > max) {
 							max = Double.valueOf((String) value);
 						}
@@ -133,8 +151,8 @@ public class DataFrame {
 
 			}
 		} else {
-			for (ArrayList<Object> arrayList : data) {
-				for (Object value : arrayList) {
+			for (ArrayList<String> arrayList : data) {
+				for (String value : arrayList) {
 					if (Double.valueOf((String) value) > max) {
 						max = Double.valueOf((String) value);
 					}
@@ -150,7 +168,7 @@ public class DataFrame {
 		if (colonne != null) {
 			for (int i = 0; i < labels.size(); i++) {
 				if (colonne.contains(labels.get(i))) {
-					for (Object value : getColumn(i)) {
+					for (String value : getColumn(i)) {
 						if (Double.valueOf((String) value) < min) {
 							min = Double.valueOf((String) value);
 						}
@@ -159,8 +177,8 @@ public class DataFrame {
 
 			}
 		} else {
-			for (ArrayList<Object> arrayList : data) {
-				for (Object value : arrayList) {
+			for (ArrayList<String> arrayList : data) {
+				for (String value : arrayList) {
 					if (Double.valueOf((String) value) < min) {
 						min = Double.valueOf((String) value);
 					}
@@ -177,7 +195,7 @@ public class DataFrame {
 		if (colonne!= null) {
 			for (int i = 0; i < labels.size(); i++) {
 				if (colonne.contains(labels.get(i))) {
-					for (Object value : getColumn(i)) {
+					for (String value : getColumn(i)) {
 						sum += Double.valueOf((String) value);
 						count++;
 					}
@@ -185,8 +203,8 @@ public class DataFrame {
 
 			}
 		} else {
-			for (ArrayList<Object> arrayList : data) {
-				for (Object value : arrayList) {
+			for (ArrayList<String> arrayList : data) {
+				for (String value : arrayList) {
 					sum += Double.valueOf((String) value);
 					count++;
 				}
@@ -195,132 +213,4 @@ public class DataFrame {
 
 		return sum/count;
 	}
-
-	public DataFrame fromCSV(String filePath) throws IOException {
-		ArrayList<ArrayList<Object>> table = new ArrayList<>();
-		BufferedReader reader = new BufferedReader(new FileReader(filePath));
-		String[] colonne = reader.readLine().split(",");
-		String lignetmp;
-		for (int i = 0; i < colonne.length; i++) {
-			labels.add(colonne[i]);
-		}
-//		reader.close();
-//		reader = new BufferedReader(new FileReader(filePath));
-		while ((lignetmp = reader.readLine()) != null) {
-			String[] ligne = lignetmp.split(",");
-			ArrayList<Object> row = new ArrayList<>();
-			for (int i = 0; i < ligne.length; i++) {
-				row.add(ligne[i]);
-			}
-			table.add(row);
-		}
-//		CSVParser parser = new CSVParser(new FileReader(filePath), CSVFormat.DEFAULT);
-//		for (CSVRecord record : parser) {
-//			ArrayList<Object> row = new ArrayList<>();
-//			for (String value : record) {
-//				row.add(value);
-//			}
-//			table.add(row);
-//		}
-		reader.close();
-		return new DataFrame(table, labels);
-	}
-
-//    String[] colonne;
-//    String[][] ligne;
-//    
-//    // construction via un tableau
-//    DataFrame(String[] colonne, String[][] ligne){
-//        this.colonne = colonne;
-//        this.ligne = new String[ligne.length][];
-//        for (int i=0; i< colonne.length; i++){
-//        	this.ligne[i] = new String[ligne[i].length];
-//            for(int j = 0; j<ligne[i].length; j++) {
-//            	this.ligne[i][j] = ligne[i][j];
-//            }
-//        }
-//        print(0,ligne.length,0,colonne.length);
-//        
-//    }
-//    
-//    // construction via un fichier csv
-//    DataFrame(File file) throws IOException {
-//    	BufferedReader reader;
-//    	int count=0;
-//    	String[] currentLine;
-//    	
-//		try {
-//            // recupere le nombre de lignes (pour initialiser la structure)
-//			reader = new BufferedReader(new FileReader(file));
-//			while(reader.readLine() != null) {
-//				count++;
-//			}
-//			count--;
-//			reader.close();
-//			
-//            // remplit la structure
-//			reader = new BufferedReader(new FileReader(file));
-//			colonne = reader.readLine().split(",");
-//			ligne = new String[count][];
-//			for(int i = 0; i<count; i++) {
-//				currentLine = reader.readLine().split(",");
-//				ligne[i] = new String[currentLine.length];
-//	            for(int j = 0; j<currentLine.length; j++) {
-//	            	ligne[i][j] = currentLine[j];
-//	            }
-//			}
-//			reader.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//			System.exit(-1);
-//		}
-//		print(0,ligne.length,0,colonne.length);
-//    }
-//    
-//    private void print(int debut_ligne, int fin_ligne, int debut_colonne, int fin_colonne) {
-//    	for(int i = debut_colonne; i<fin_colonne; i++) {
-//    		System.out.print("  "+colonne[i]);
-//    	}
-//        
-//        System.out.println();
-//        
-//        for (int j=debut_ligne; j<fin_ligne; j++){
-//            System.out.print(j+" ");
-//            for (int i=debut_colonne; i<fin_colonne; i++){
-//            	System.out.print(ligne[j][i]+" ");
-//            }
-//            System.out.println();
-//        }
-//    }
-//
-//	public void printAll() {
-//		print(0, ligne.length, 0, colonne.length);
-//	}
-//    
-//	public void printRows(int debut_ligne, int fin_ligne) {
-//		print(debut_ligne, fin_ligne, 0, colonne.length);
-//	}
-//
-//	public void printColumns(int debut_colonne, int fin_colonne) {
-//		print(0, ligne.length, debut_colonne, fin_colonne);
-//	}
-//
-//	public boolean equals(DataFrame df) {
-//		if(this.colonne.length != df.colonne.length) {
-//			return false;
-//		}
-//		// vérfie la taille de chaque ligne
-//		for(int i = 0; i < ligne.length; i++) {
-//			if(this.ligne[i].length != df.ligne[i].length) {
-//				return false;
-//			}
-//			// vérifie chaque élément
-//			for(int j = 0; j < colonne.length; j++) {
-//				if(!(this.ligne[j][i].equals(df.ligne[j][i]))) {
-//					return false;
-//				}
-//			}
-//		}
-//		return true;
-//	}
 }
--- pelican/src/test/java/devops/DataFrameTest.java
+++ pelican/src/test/java/devops/DataFrameTest.java
@@ -2,9 +2,6 @@ package devops;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -17,35 +14,54 @@ public class DataFrameTest
     {
         ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
         String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
-        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
         for(int i = 0; i < lines.length; i++){
-            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
         }
         DataFrame dataFrame = new DataFrame(table, column);
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFramefromCSVCorrect(){
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"1", "2", "3", "4"},{"4","3","2","1"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+    try {
+        DataFrame fromCsV = new DataFrame("src/test/resources/test.csv");
+        assertEquals("line 1", dataFrame.getLigne(0), fromCsV.getLigne(0));
+        assertEquals("line 2", dataFrame.getLigne(1), fromCsV.getLigne(1));
+
+    } catch (Exception e) {
+        System.out.println("File opening problem");
+    }
     }
 
     @Test(timeout = DEFAULT_TIMEOUT)
     public void DataFramegetColumnCorrect() {
         ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
         String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
-        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
         for(int i = 0; i < lines.length; i++){
-            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
         }
         DataFrame dataFrame = new DataFrame(table, column);
-        assertEquals("column 1", new ArrayList<Object>(Arrays.asList("E","I")), dataFrame.getColumn(0));
-        assertEquals("column 2", new ArrayList<Object>(Arrays.asList("F","J")), dataFrame.getColumn(1));
-        assertEquals("column 3", new ArrayList<Object>(Arrays.asList("G","K")), dataFrame.getColumn(2));
-        assertEquals("column 4", new ArrayList<Object>(Arrays.asList("H","L")), dataFrame.getColumn(3));
+        assertEquals("column 1", new ArrayList<String>(Arrays.asList("E","I")), dataFrame.getColumn(0));
+        assertEquals("column 2", new ArrayList<String>(Arrays.asList("F","J")), dataFrame.getColumn(1));
+        assertEquals("column 3", new ArrayList<String>(Arrays.asList("G","K")), dataFrame.getColumn(2));
+        assertEquals("column 4", new ArrayList<String>(Arrays.asList("H","L")), dataFrame.getColumn(3));
 	}
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testIndexOutOfBoundsExceptionDataFrameGetColumnCorrect() {
         ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
         String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
-        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
         for(int i = 0; i < lines.length; i++){
-            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
         }
         DataFrame dataFrame = new DataFrame(table, column);
         dataFrame.getColumn(5);
@@ -55,24 +71,152 @@ public class DataFrameTest
     public void DataFramegetLineCorrect() {
         ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
         String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
-        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
         for(int i = 0; i < lines.length; i++){
-            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
         }
         DataFrame dataFrame = new DataFrame(table, column);
-        assertEquals("column 1", new ArrayList<Object>(Arrays.asList("E","F", "G", "H")), dataFrame.getLigne(0));
-        assertEquals("column 2", new ArrayList<Object>(Arrays.asList("I","J", "K", "L")), dataFrame.getLigne(1));
+        assertEquals("line 1", new ArrayList<String>(Arrays.asList("E","F", "G", "H")), dataFrame.getLigne(0));
+        assertEquals("line 2", new ArrayList<String>(Arrays.asList("I","J", "K", "L")), dataFrame.getLigne(1));
 	}
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testIndexOutOfBoundsExceptionDataFrameGetLineCorrect() {
         ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
         String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
-        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
         for(int i = 0; i < lines.length; i++){
-            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
         }
         DataFrame dataFrame = new DataFrame(table, column);
-        assertEquals("column 1", new ArrayList<Object>(Arrays.asList("E","F", "G", "H")), dataFrame.getLigne(3));
+        dataFrame.getLigne(3);
 	}
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFramegetLabelCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+        assertEquals("column 1", "A", dataFrame.getLabel(0));
+        assertEquals("column 2", "B", dataFrame.getLabel(1));
+        assertEquals("column 3", "C", dataFrame.getLabel(2));
+        assertEquals("column 4", "D", dataFrame.getLabel(3));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsExceptionDataFrameGetLabelCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+        dataFrame.getLabel(4);
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFrameaddRowCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+        dataFrame.addRow(new ArrayList<String>(Arrays.asList("M", "N", "O", "P")));
+        assertEquals("added the last line", Arrays.asList("M", "N", "O", "P"), dataFrame.getLigne(2));
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFrameSelectionLignesCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"},{"M","N","O","P"},{"Q","R","S","T"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+
+        DataFrame extracted = dataFrame.selectionLignes(new ArrayList<Integer>(Arrays.asList(1,3)));
+        assertEquals("line 1 equals line 1", dataFrame.getLigne(1), extracted.getLigne(0));
+        assertEquals("line 2 equals line 4", dataFrame.getLigne(3), extracted.getLigne(1));
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFrameSelectionColonneCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"},{"M","N","O","P"},{"Q","R","S","T"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+
+        DataFrame extracted = dataFrame.selectionColonne(new ArrayList<String>(Arrays.asList("A","C")));
+        assertEquals("column 1 equals line 1", dataFrame.getColumn(0), extracted.getColumn(0));
+        assertEquals("column 3 equals line 2", dataFrame.getColumn(2), extracted.getColumn(1));
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFramemaxValueCorrect(){
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"1", "2", "3", "4"},{"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column); 
+
+        Double result = dataFrame.maxValue(new ArrayList<String>(Arrays.asList("A","C")));
+        assertEquals("Max value between A and C", Double.valueOf("15"), result);
+        result = dataFrame.maxValue(new ArrayList<String>(Arrays.asList("C","D")));
+        assertEquals("Max value between C and D", Double.valueOf("16"), result);
+        result = dataFrame.maxValue(null);
+        assertEquals("Max value in tab", Double.valueOf("16"), result);
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFrameminValueCorrect(){
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"1", "2", "3", "4"},{"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column); 
+
+        Double result = dataFrame.minValue(new ArrayList<String>(Arrays.asList("A","C")));
+        assertEquals("Min value between A and C", Double.valueOf("1"), result);
+        result = dataFrame.minValue(new ArrayList<String>(Arrays.asList("C","D")));
+        assertEquals("Min value between C and D", Double.valueOf("3"), result);
+        result = dataFrame.minValue(null);
+        assertEquals("min value of tab", Double.valueOf("1"), result);
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFramemoyenValueCorrect(){
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"1", "2", "3", "4"},{"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<String>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column); 
+        Double real=0.;
+        for(int i = 1; i < 17; i++){
+            real += i;
+        }
+        real /= 16.;
+        Double result = dataFrame.moyenValue(null);
+        assertEquals("Mean of the table", real, result);
+        real = (3+7+11+15+4+8+12+16)/8.;
+        result = dataFrame.moyenValue(new ArrayList<String>(Arrays.asList("C","D")));
+        assertEquals("Mean of the table", real, result);
+
+    }
 }
