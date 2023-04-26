d66e933252d7956c4fa38f766cdbacc29b99af8a
Jean-Edouard <Jean-Edouard.Bournat-Querat@etu.univ-grenoble-alpes.fr>
2023-04-26 21:59:09 +0200

    test getcolumn getline

--- pelican/src/test/java/devops/DataFrameTest.java
+++ pelican/src/test/java/devops/DataFrameTest.java
@@ -5,33 +5,74 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataFrameTest 
 {
     private static final int DEFAULT_TIMEOUT = 2000;
 
     @Test(timeout = DEFAULT_TIMEOUT)
-    public void DataFrameStringFromArrayTest()
+    public void DataFrameStringFromArraylistTest()
     {
-        String[] column = {"A", "B", "C", "D"};
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
         String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
-        DataFrame dataFrame = new DataFrame(column, lines);
+        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
     }
-
 
     @Test(timeout = DEFAULT_TIMEOUT)
-    public void DataFrameStringEqualsTest()
-    {
-        String[] c = {"a", "b"};
-        String[][] l = {{"aa","bb"},{"cc","dd"}};
-        DataFrame dfarray = new DataFrame(c, l);
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        File test = new File("src/test/resources/test.csv");
-        try {
-            DataFrame dffile = new DataFrame(test);
-            assertTrue("Dataframes are different", dfarray.equals(dffile));
-        } catch (IOException e) {
-            System.out.println(e.toString());
+    public void DataFramegetColumnCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
+        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
         }
+        DataFrame dataFrame = new DataFrame(table, column);
+        assertEquals("column 1", new ArrayList<Object>(Arrays.asList("E","I")), dataFrame.getColumn(0));
+        assertEquals("column 2", new ArrayList<Object>(Arrays.asList("F","J")), dataFrame.getColumn(1));
+        assertEquals("column 3", new ArrayList<Object>(Arrays.asList("G","K")), dataFrame.getColumn(2));
+        assertEquals("column 4", new ArrayList<Object>(Arrays.asList("H","L")), dataFrame.getColumn(3));
+	}
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsExceptionDataFrameGetColumnCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
+        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+        dataFrame.getColumn(5);
     }
+
+    @Test(timeout = DEFAULT_TIMEOUT)
+    public void DataFramegetLineCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
+        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+        assertEquals("column 1", new ArrayList<Object>(Arrays.asList("E","F", "G", "H")), dataFrame.getLigne(0));
+        assertEquals("column 2", new ArrayList<Object>(Arrays.asList("I","J", "K", "L")), dataFrame.getLigne(1));
+	}
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsExceptionDataFrameGetLineCorrect() {
+        ArrayList<String> column = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
+        String[][] lines = {{"E", "F", "G", "H"},{"I","J","K","L"}};
+        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        for(int i = 0; i < lines.length; i++){
+            table.add(new ArrayList<Object>(Arrays.asList(lines[i])));
+        }
+        DataFrame dataFrame = new DataFrame(table, column);
+        assertEquals("column 1", new ArrayList<Object>(Arrays.asList("E","F", "G", "H")), dataFrame.getLigne(3));
+	}
 }
